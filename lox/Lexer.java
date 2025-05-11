a0e3e7c78e7fc332206689ff4540aebdc2b9f7ba
hghit-harshit <144206069+hghit-harshit@users.noreply.github.com>
2025-05-11 18:50:32 +0530

    fix : fixed slash not lexing properly

--- lox/Lexer.java
+++ lox/Lexer.java
@@ -102,6 +102,7 @@ public class Lexer
                 {
                     addToken(TokenType.SLASH);
                 }
+                break;
             case '"' : string();break;
 
             default : 
