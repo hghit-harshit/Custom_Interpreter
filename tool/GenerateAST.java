ad3972028ec3af749023297f00bdc60b95827c40
hghit-harshit <144206069+hghit-harshit@users.noreply.github.com>
2025-05-16 23:21:31 +0530

    feat : added statements

--- tool/GenerateAST.java
+++ tool/GenerateAST.java
@@ -21,10 +21,19 @@ public class GenerateAST
 
         String outputDir = args[0];
         defineAST(outputDir, "Expr", Arrays.asList(
-         "Binary : Expr left, Token operator, Expr right",
-            "Grouping : Expr expression",
-            "Literal  : Object value",
-            "Unary    : Token operator, Expr right"
+            "Assign : Token name, Expr value",
+            "Binary      : Expr left, Token operator, Expr right",
+            "Grouping    : Expr expression",
+            "Literal     : Object value",
+            "Unary       : Token operator, Expr right",
+            "Variable    : Token name "
+        )); 
+
+        defineAST(outputDir, "Stmt", Arrays.asList(
+            "Block : List<Stmt> statements",
+            "Expression : Expr expression",
+            "Print      : Expr expression",
+            "Var        : Token name, Expr initializer"
         ));
     }
     
